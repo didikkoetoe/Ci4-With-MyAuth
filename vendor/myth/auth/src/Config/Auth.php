41579ac965f13bd17795a341acfe8fafcdc75d89
didikkoetoe <danieldidikprabowo@gmail.com>
2021-10-14 19:58:44 +0700

    mangarahkan view MyAuth ke view kita

--- vendor/myth/auth/src/Config/Auth.php
+++ vendor/myth/auth/src/Config/Auth.php
@@ -35,8 +35,10 @@ class Auth extends BaseConfig
 	 * @var array
 	 */
 	public $views = [
-		'login'		   => 'Myth\Auth\Views\login',
-		'register'		=> 'Myth\Auth\Views\register',
+		// 'login'		   => 'Myth\Auth\Views\login',
+		// 'register'		=> 'Myth\Auth\Views\register',
+		'login'		   => '\App\Views\auth\login',
+		'register'		=> '\App\Views\auth\register',
 		'forgot'		  => 'Myth\Auth\Views\forgot',
 		'reset'		   => 'Myth\Auth\Views\reset',
 		'emailForgot'	 => 'Myth\Auth\Views\emails\forgot',
