9e43b858689a87cf17274e01f045d4fc581949c3
didikkoetoe <danieldidikprabowo@gmail.com>
2021-10-15 19:14:57 +0700

    Menambahkan configurasi di Email

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
     /**
      * @var string
      */
-    public $fromEmail;
+    public $fromEmail = 'didik.yayasanpeduli@gmail.com';
 
     /**
      * @var string
      */
-    public $fromName;
+    public $fromName = 'didik prabowo';
 
     /**
      * @var string
