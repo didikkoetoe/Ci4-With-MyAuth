0b11ff4a89f5f2ca2feb63f810a5b449ccf97735
didikkoetoe <danieldidikprabowo@gmail.com>
2021-10-15 19:01:47 +0700

    Edit view login MythAuth

--- vendor/myth/auth/src/Views/login.php
+++ vendor/myth/auth/src/Views/login.php
@@ -45,7 +45,8 @@
 <?php if ($config->allowRemembering): ?>
 						<div class="form-check">
 							<label class="form-check-label">
-								<input type="checkbox" name="remember" class="form-check-input" <?php if(old('remember')) : ?> checked <?php endif ?>>
+								<input type="checkbox" name="remember" class="form-check-input" <?php if(old('remember')) : ?> checked
+<?php endif ?>>
 								<?=lang('Auth.rememberMe')?>
 							</label>
 						</div>
