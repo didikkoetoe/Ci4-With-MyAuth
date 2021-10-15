e7473d1f3395c27db0b17c67b34716a02676346f
didikkoetoe <danieldidikprabowo@gmail.com>
2021-10-15 19:11:17 +0700

    Mengambil title dari controller

--- app/Views/templates/index.php
+++ app/Views/templates/index.php
@@ -7,7 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Didik Prabowo - Profile</title>
+    <title>
+        <?= $title; ?>
+    </title>
     <!-- Custom fonts for this template-->
     <link href="<?= base_url(); ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -64,7 +66,7 @@
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a class="btn btn-primary" href="<?= site_url('logout'); ?>">Logout</a>
                 </div>
             </div>
         </div>
