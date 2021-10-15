cd5a41c81f3706322928ad9e775df18594f53185
didikkoetoe <danieldidikprabowo@gmail.com>
2021-10-15 19:12:10 +0700

    Menambahakn form di view login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -11,30 +11,60 @@
                         <div class="col-12">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    <h1 class="h4 text-gray-900 mb-4">
+                                        <?=lang('Auth.loginTitle')?>
+                                    </h1>
                                 </div>
-                                <form class="user">
+                                <?= view('Myth\Auth\Views\_message_block') ?>
+                                <form action="<?= route_to('login') ?>" method="post" class="user">
+                                    <?= csrf_field() ?>
+                                    <?php if ($config->validFields === ['email']): ?>
+                                    <!-- Email -->
                                     <div class="form-group">
-                                        <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox small">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck">
-                                            <label class="custom-control-label" for="customCheck">Remember
-                                                Me</label>
+                                        <input type="email" class="form-control form-control-user <?php if(session('errors.email')) : ?>is-invalid<?php endif ?>" name="email" placeholder="<?=lang('Auth.email')?>" value="<?= old('email') ?>">
+                                        <div class="invalid-feedback">
+                                            <?= session('errors.login') ?>
                                         </div>
                                     </div>
-                                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                        Login
-                                    </a>
+                                    <?php else: ?>
+                                    <!-- Username -->
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-user <?php if(session('errors.login')) : ?>is-invalid<?php endif ?>" name="login" placeholder="<?=lang('Auth.emailOrUsername')?>">
+                                        <div class="invalid-feedback">
+                                            <?= session('errors.login') ?>
+                                        </div>
+                                    </div>
+                                    <?php endif; ?>
+                                    <!-- Password -->
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user <?php if(session('errors.password')) : ?>is-invalid<?php endif ?>" name="password" placeholder="<?=lang('Auth.password')?>" autocomplete="off">
+                                        <div class="invalid-feedback">
+                                            <?= session('errors.password') ?>
+                                        </div>
+                                    </div>
+                                    <!-- Remember Me -->
+                                    <?php if ($config->allowRemembering): ?>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox small">
+                                            <input type="checkbox" class="custom-control-input" name="remember" <?php if(old('remember')) : ?> checked
+                                            <?php endif ?>>
+                                            <label class="custom-control-label" for="customCheck">
+                                                <?=lang('Auth.rememberMe')?></label>
+                                        </div>
+                                    </div>
+                                    <?php endif; ?>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        <?=lang('Auth.loginAction')?></button>
                                 </form>
                                 <hr>
-                                <div class="text-center">
-                                    <a class="small" href="register.html">Create an Account!</a>
-                                </div>
+                                <?php if ($config->allowRegistration) : ?>
+                                <p><a href="<?= route_to('register') ?>">
+                                        <?=lang('Auth.needAnAccount')?></a></p>
+                                <?php endif; ?>
+                                <?php if ($config->activeResetter): ?>
+                                <p><a href="<?= route_to('forgot') ?>">
+                                        <?=lang('Auth.forgotYourPassword')?></a></p>
+                                <?php endif; ?>
                             </div>
                         </div>
                     </div>
